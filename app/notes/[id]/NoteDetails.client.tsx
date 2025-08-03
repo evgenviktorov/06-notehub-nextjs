de189d43092c7ab310bd66e31d6f1477ea5553b9
EV <evgenviktorov88@gmail.com>
2025-08-03 19:56:50 +0300

    work-in-progress-bugfix-2

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
 import { fetchNoteById } from '@/lib/api'
-import css from './NoteDetails.module.css'
+import { useQuery } from '@tanstack/react-query'
+import css from './NoteDetails.client.module.css'
 
 interface Props {
 	id: string
